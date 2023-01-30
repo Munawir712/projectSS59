9d659be10972273a849e983628613cceee1c60f9
Munawir712 <munawirjr2@gmail.com>
2023-01-30 09:29:24 +0700

    feat:Menambah Controller Penyewa dan route penyewa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenyewaController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function() {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [PenyewaController::class, 'index'])->name('penyewa');
+});
